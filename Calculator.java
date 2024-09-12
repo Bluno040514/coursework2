d34c13e704e59e20cfb07367c1025b4e173c821c
Bluno040514 <1754414234@qq.com>
2024-09-12 12:42:19 +0800

    Initial commit

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+        public static void main(String[] args) {
+            int a = 10;
+            int b = 5;
+
+            int sum = a + b;
+            int difference = a - b;
+            int quotient = a / b;  // New division feature
+
+            System.out.println("Sum: " + sum);
+            System.out.println("Difference: " + difference);
+            System.out.println("Quotient: " + quotient);  // Output for division
+        }
+    }
+
+
+
+
