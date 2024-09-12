190b2ec9cd956e5ac1798947ec6e47c3c473f875
Bluno040514 <1754414234@qq.com>
2024-09-12 13:10:30 +0800

    Added division

--- Calculator.java
+++ Calculator.java
@@ -1,17 +1,17 @@
 public class Calculator {
-        public static void main(String[] args) {
-            int a = 10;
-            int b = 5;
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
 
-            int sum = a + b;
-            int difference = a - b;
-            int quotient = a / b;  // New division feature
+        int sum = a + b;
+        int difference = a - b;
+        int quotient = a / b;  // New division feature
 
-            System.out.println("Sum: " + sum);
-            System.out.println("Difference: " + difference);
-            System.out.println("Quotient: " + quotient);  // Output for division
-        }
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Quotient: " + quotient);  // Output for division
     }
+}
 
 
 
